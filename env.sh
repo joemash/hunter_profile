91d1a11da29a145bed4677daa1cf3c7abdd24d08
josephat <josephat.macharia@savannahinformatics.com>
2019-02-26 10:20:02 +0300

    removed hardening.yml

--- env.sh
+++ env.sh
@@ -4,4 +4,4 @@ export DATABASE_URL='postgres://postgres:chinoxl@localhost:5432/hunter_db'
 export DJANGO_SECRET_KEY='8yll3$$$omye0+0v=tfb--el9&nqlrppgb96b*21p5isc_#s0u'
 export DJANGO_ALLOWED_HOSTS='evoconsultants.co.ke'
 export REDIS_URL='//redis'
-DJANGO_ADMIN_URL
+export DJANGO_ADMIN_URL='/admin'
